047d6a735dbe9be8e6c5d9bc1fe081daa092e51a
Manthan Shingala <shingalamanthan@gmail.com>
2024-12-25 14:55:19 +0530

    This file in "colors" named folder

---
+++ C.java
@@ -0,0 +1,23 @@
+package colors;
+
+public class C{
+    public static final String reset = "\u001B[0m";
+    public static final String fBlack = "\u001B[30m";
+    public static final String bgBlack = "\u001B[40m";
+    public static final String fRed = "\u001B[31m";
+    public static final String bgRed = "\u001B[41m";
+    public static final String fGreen = "\u001B[32m";
+    public static final String bgGreen = "\u001B[42m";
+    public static final String fYellow = "\u001B[33m";
+    public static final String bgYellow = "\u001B[43m";
+    public static final String fBlue = "\u001B[34m";
+    public static final String bgBlue = "\u001B[44m";
+    public static final String fMagenta = "\u001B[35m";
+    public static final String bgMagenta = "\u001B[45m";
+    public static final String fCyan = "\u001B[36m";
+    public static final String bgCyan = "\u001B[46m";
+    public static final String fWhite = "\u001B[37m";
+    public static final String bgWhite = "\u001B[47m";
+    public static final String fGray = "\u001B[90m";
+    public static final String bgGray = "\u001B[100m";
+}
